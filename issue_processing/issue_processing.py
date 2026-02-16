a1cfe3137c2bdbf8b6da0298637a358182972359
RitikaHiremath <ritika.hiremath@gmail.com>
2026-02-16 11:46:02 +0100

    updated copyright for logging import

--- issue_processing/issue_processing.py
+++ issue_processing/issue_processing.py
@@ -21,6 +21,7 @@
 # Copyright 2025-2026 by Leo Sendelbach <s8lesend@stud.uni-saarland.de>
 # Copyright 2026 by Thomas Bock <bockthom@cmu.edu>
 # Copyright 2025 by Maximilian Löffler <s8maloef@stud.uni-saarland.de>
+# Copyright 2025-2026 by Ritika Hiremath <rihi00002@stud.uni-saarland.de>
 # All Rights Reserved.
 """
 This file is able to extract Github issue data from json files.
