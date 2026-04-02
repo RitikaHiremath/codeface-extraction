3164ddf037967f936aaf894fb33f776ea7a762dc
RitikaHiremath <ritika.hiremath@gmail.com>
2026-04-02 10:18:20 +0200

    copyright updated

--- run-zulip-issues.py
+++ run-zulip-issues.py
@@ -1,3 +1,19 @@
+# coding=utf-8
+# This file is part of codeface-extraction, which is free software: you
+# can redistribute it and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright 2025-2026 by Ritika Hiremath <rihi00002@stud.uni-saarland.de>
+# All Rights Reserved.
 import issue_processing.zulip_issue_processing as zulip_issues
 
 zulip_issues.run()
